65f01938e6a2e721d11db2f2f77befb63859ac90
Dean Baker <dean.baker@digio.com.au>
2019-09-03 20:05:34 +1000

    Adding rolling totals

--- src/main/java/com/digio/kafka/transactions/StreamingTopology.java
+++ src/main/java/com/digio/kafka/transactions/StreamingTopology.java
@@ -50,7 +50,17 @@ public class StreamingTopology {
 
     public static KStream<Windowed<String>, Long> computeRunningTotal(KStream<String, Transaction> kStream) {
 
-        return null;
+        long windowSizeMs = TimeUnit.SECONDS.toMillis(30);
+        TimeWindows weeklySpendWindow = TimeWindows.of(windowSizeMs);
+
+        return kStream
+                .groupByKey()
+                .windowedBy(weeklySpendWindow)
+                .aggregate(
+                        () -> 0L,
+                        (key, value, aggregate) -> aggregate + value.getAmount(),
+                        Materialized.with(new Serdes.StringSerde(), new Serdes.LongSerde()))
+                .toStream();
     }
 
    public static GlobalKTable<String, String> createCategoryLookupTable(StreamsBuilder builder) {
