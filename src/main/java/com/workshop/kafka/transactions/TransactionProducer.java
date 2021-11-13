9987a4647a38c6916b25f266e3aa47fd7ba4b508
Dean Baker <dean.baker@pexa.com.au>
2021-11-13 15:35:44 +1100

    Fixing imports

--- src/main/java/com/workshop/kafka/transactions/TransactionProducer.java
+++ src/main/java/com/workshop/kafka/transactions/TransactionProducer.java
@@ -1,14 +1,19 @@
 package com.workshop.kafka.transactions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.stream.IntStream;
 
 /**
  * TransactionProducer will publish transactions
